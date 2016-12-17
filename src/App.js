cad9f3f9e3a4b401e05ab87f13d5afa2f6f90040
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-12-17 12:44:01 +0100

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+ class Header extends Component {
+  render() {
+    return (
+          <nav className="navbar navbar-fixed-top navbar-inner header">
+
+            <div className="container">
+              <div className="navbar-brand">
+                <a href="#" > Suryanarayanamurthy </a>
+              </div>
+              <ul className="nav navbar-nav pull-right">
+                <li>
+                  <a href='#' className="nav-link" target="_blank" style={{ lineHeight: 'normal' }}>
+                    <span>Resume</span>
+                  </a>
+                </li>
+                <li>
+                  <a href='#' className="nav-link">
+                    <i className="fa fa-envelope icon"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href='https://github.com/Suryanarayanamurthy' className="nav-link" target="_blank">
+                    <i className="fa fa-github icon"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href='https://de.linkedin.com/in/suryanarayanamurthy1' className="nav-link" target="_blank">
+                    <i className="fa fa-linkedin-square icon"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href='https://angel.co/suryanarayanamurthy-hanumantha-vajjhala' className="nav-link" target="_blank">
+                    <i className="fa fa-angellist icon"></i>
+                  </a>
+                </li>
+                </ul>
+            </div>
+            </nav>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <Header/>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
