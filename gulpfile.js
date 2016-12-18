7a0deedc18e74c7c888c15ec6487059f2df98d27
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-12-18 18:57:16 +0100

    add gulp task 'styles' to convert scss to css

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');  
+var sass = require('gulp-sass');
+
+//style paths
+var sassFiles = 'src/styles/*.scss',  
+    cssDest = 'src/';
+
+gulp.task('styles', function(){  
+    gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
