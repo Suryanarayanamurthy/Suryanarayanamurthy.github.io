5c797df3165d2e0b49e8835a16a5afbe707d0ec8
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-12-19 22:42:43 +0100

    add pomodoro project to portfolio.

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [{
       date: '2016',
       github_url: 'https://github.com/Suryanarayanamurthy/Suryanarayanamurthy.github.io',
       project_url: 'https://suryanarayanamurthy.herokuapp.com/',
-      background: 'This is my portfolio project',
+      background: "This is my 3rd Iteration of creating my portfolio .This time, I purged my old portfolio and did a brand one starting from 'create-react-app'",
       role: "\" The Developer\"",
       tech_stack: ['React.js', 'Heroku']
 
@@ -12,11 +12,12 @@ const projectsData = [{
       title: 'Pomodoro App',
       date: '2016',
       github_url: 'https://github.com/Suryanarayanamurthy/pomodoro',
-      project_url: 'http://codepen.io/Suryanarayanamurthy/pen/LpQOJW',
-      background: 'This is a Pomodoroapp',
-      role: "\" The Developer\"",
+      project_url: 'http://codepen.io/Suryanarayanamurthy/full/LpQOJW/',
+      background: 'The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals traditionally 25 minutes in length, separated by short breaks. These intervals are known as "pomodoros", the plural in English of the Italian word pomodoro meaning "tomato". The method is based on the idea that frequent breaks can improve mental agility.',
+      role: "Front End Developer",
       tech_stack: ['AngularJS']
 
-}
+},
+
 ];
 export {projectsData};
