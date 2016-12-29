721c03cdb6258715b3c63ef4df0f59f849adc674
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-12-29 20:54:27 +0100

    open url links in seperate tab

--- src/components/resumeComponents/Profile.js
+++ src/components/resumeComponents/Profile.js
@@ -8,16 +8,16 @@ const Profile = props => {
                   <h2 className="text-center">{profileObj.label}</h2>
                   <div className="divider"></div>
                   <ul className="list-unstyled contact-links text-center">
-                    <li><i className="fa fa-lg fa-location-arrow"></i>{profileObj.location.city}, {profileObj.location.region}, {profileObj.location.countryCode}</li>
-                    <li><i className="fa fa-lg fa-envelope"></i><a href="mailto:{profileObj.email}">{profileObj.email}</a></li>
+                    <li><i className="fa fa-lg fa-location-arrow"></i>{profileObj.location.city} {/*,{profileObj.location.region}*/}, {profileObj.location.countryCode}</li>
+                    <li><i className="fa fa-lg fa-envelope"></i><a href={"mailto:"+profileObj.email}>{profileObj.email}</a></li>
                   </ul>
                   <div className="divider"></div>
                   <ul className="profileLinks list-inline text-center">
                     {/*<li><a className="fa fa-twitter fa-2x" href={'https://twitter.com/'+profileObj.profiles[0].username}></a></li>*/}
-                    <li><a className="fa fa-github fa-2x" href={'https://github.com/'+profileObj.profiles[0].username}></a></li>
+                    <li><a className="fa fa-github fa-2x" href={'https://github.com/'+profileObj.profiles[0].username} target="_blank"></a></li>
                   </ul>
                   <div className="divider"></div>
-                  <p>I built this page with <a href="https://facebook.github.io/react/">React</a> components and a <a href="https://jsonresume.org/schema/">JSON Resume Schema</a>. The full source code can be found in <a href="https://github.com/Suryanarayanamurthy">my Github repo</a>.</p>
+                  <p>I built this page with <a href="https://facebook.github.io/react/" target="_blank">React</a> components and a <a href="https://jsonresume.org/schema/" target="_blank">JSON Resume Schema</a>. The full source code can be found in <a href="https://github.com/Suryanarayanamurthy/Suryanarayanamurthy.github.io" target="_blank">my Github repo</a>.</p>
             </div>
 };
 
